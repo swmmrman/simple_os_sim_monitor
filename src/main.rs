9a174ec99db2468c044bc728a217162a1df4b429
Stephan Kern <stephan@swmmrman.com>
2022-10-30 02:48:46 -0600

    Struct to load into

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use toml;
 use std::fs::File;
 use std::path::Path;
 
+#[derive(Debug)]
+struct sim_config {
+    protocol: String,
+    server_name: String,
+    port: u32,
+    end_point: String,
+}
+
 fn main() {
     let fh = File::open(Path::new("siminfo.toml"));
     println!("Hello, world!");
